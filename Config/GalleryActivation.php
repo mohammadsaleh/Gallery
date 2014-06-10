6e5c27bb9bc350cb78c14c3404f25705c8e699d2
moharami <danialamiri2002@yahoo.com>
2014-06-10 12:35:29 +0430

    on activation and on deactivation

--- Config/GalleryActivation.php
+++ Config/GalleryActivation.php
@@ -14,19 +14,35 @@ class GalleryActivation {
             $setting->write('Gallery.small_height', '50', array(
                 'editable' => '1'
             ));
+            $setting->write('Gallery.medium_width', '255', array(
+                'editable' => '1'
+            ));
+            $setting->write('Gallery.medium_height', '170', array(
+                'editable' => '1'
+            ));
+            $setting->write('Gallery.large_width', '0', array(
+                'editable' => '1'
+            ));
+            $setting->write('Gallery.large_height', '533', array(
+                'editable' => '1'
+            ));
             
             return true;
 	}
 
-	// public function beforeDeactivation(&$controller) {
- //            return true;
-	// }
+	public function beforeDeactivation(&$controller) {
+            return true;
+	}
 
-	// public function onDeactivation(&$controller) {
- //            $setting = ClassRegistry::init('Setting');
- //            $setting->deleteKey('aparnicTemplate.rtl');
- //            $setting->deleteKey('aparnicTemplate.theme');
- //            return true;
-	// }
+	public function onDeactivation(&$controller) {
+            $setting = ClassRegistry::init('Setting');
+            $setting->deleteKey('Gallery.small_width');
+            $setting->deleteKey('Gallery.small_height');
+            $setting->deleteKey('Gallery.medium_width');
+            $setting->deleteKey('Gallery.medium_height');
+            $setting->deleteKey('Gallery.large_width');
+            $setting->deleteKey('Gallery.large_height');
+            return true;
+	}
 
 }
